9a46439faa8be78b6048febc6299173a5ec7c160
iZafor <izafor65@gmail.com>
2024-05-06 15:58:35 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "./components/mode-toggle"
+
+export default function App() {
+  return (
+    <div>
+      <div className="w-full grid justify-end pr-4 mt-4">
+        <ModeToggle />
+      </div>
+    </div>
+  );
+}
