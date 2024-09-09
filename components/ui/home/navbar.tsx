2a6b713384c3337602363753a9d8a93c4dd4f13d
iZafor <izafor65@gmail.com>
2024-09-09 21:09:45 +0600

    replace user icon with a direct logout option

--- components/ui/home/navbar.tsx
+++ components/ui/home/navbar.tsx
@@ -1,15 +1,32 @@
 "use client";
 
 import { ThemeToggle } from "../theme-toggle";
-import UserMenu from "./user-menu";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { STORED_AUTH_DATA_KEY } from "@/lib/definition";
+import { LogOut } from "lucide-react";
+import { Button } from "../button";
 
 export default function Navbar() {
     const pathname = usePathname();
+    const router = useRouter();
+
+    function handleLogout() {
+        localStorage.removeItem(STORED_AUTH_DATA_KEY);
+        router.push("/");
+    }
 
     return (
-        <header className="flex items-center justify-end h-16 px-4">
-            {pathname === "/dashboard" && <UserMenu />}
+        <header className="flex items-center justify-end gap-2 h-16 px-2 max-sm:px-[.6rem]">
+            {pathname === "/dashboard" && (
+                <Button 
+                    size="icon" 
+                    variant="outline" 
+                    onClick={handleLogout}
+                >
+                    <LogOut />
+                </Button>
+            )}
             <ThemeToggle />
         </header>
     );
